caa84bd2eaadac6e16994226d239a56a41098180
Michael Yeaney <michael.yeaney@gmail.com>
2016-08-22 11:13:05 -0400

    Stubs for application structure...still re-considering overall layout.

---
+++ services/application.js
@@ -0,0 +1,15 @@
+/*
+
+Contains the main application logic/workflow
+
+*/
+
+(function(scope){
+    // Make container for applicaiton
+    if (!scope.Application){
+        scope.Application = {};
+    }
+
+    // Export methods
+    
+})(this);
